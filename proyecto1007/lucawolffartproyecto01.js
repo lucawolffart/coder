41cd195a838824dfc12946cdcd8401f30a85bed3
Luca Wolffart <100728626+lucawolffart@users.noreply.github.com>
2022-07-13 18:36:46 -0300

    Add files via upload

--- proyecto1007/lucawolffartproyecto01.js
+++ proyecto1007/lucawolffartproyecto01.js
@@ -1,6 +1,41 @@
 alert("Hola, escoja entre los siguientes productos")
 
-const productos = ["cocacola", " pepsi", " chocolate"]
+class productos {
+    constructor(nombre, precio, id) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+
+    }
+}
+
+const cocacola = new productos("cocacola",
+ 200,
+ 111,
+);
+
+const pepsi = new productos(
+    "pepsi",
+    200,
+    222,
+)
+
+const chocolate = new productos (
+    "chocolate",
+    500,
+    333,
+)
+
+const helado = new productos(
+    "helado",
+    600,
+    444,
+)
+
+
+//------------------Tareas anteriores----------------
+
+/*const productos = ["cocacola", " pepsi", " chocolate"]
 
 productos.push (" helado")
 
@@ -8,33 +43,35 @@ productos.push (" helado")
 •pepsi
 •chocolate`)*/
 
-let a = prompt(productos)
+//---------------------------------------------------
+
+let a = prompt((cocacola.nombre) + ", " + (chocolate.nombre) + ", " + (pepsi.nombre) + ", " + (helado.nombre))
 
 
 if ( a == " ") {
     alert("Ingrese un producto");
 } 
-else if (a == "cocacola") {
-    alert("El producto cuesta $200");
+else if (a == "cocacola") { 
+    alert("el producto cuesta $" + (cocacola.precio));
     alert("¡Que lo disfrutes!")
     alert(`oh ¿aun no te enteraste?
     hay un codigo de descuento. Se encuentra escondido en la consola`)
 } 
 else if ( a == "pepsi") {
-    alert("El producto cuesta $200");
+    alert("El producto cuesta $" + (pepsi.precio));
     alert("¡Que lo disfrutes!")
     alert(`oh ¿aun no te enteraste?
     hay un codigo de descuento. Se encuentra escondido en la consola`)
 } 
 else if (a == "chocolate") {
-    alert("el producto cuesta $500");
+    alert("el producto cuesta $" + (chocolate.precio));
     alert("¡Que lo disfrutes!")
     alert(`oh ¿aun no te enteraste?
     hay un codigo de descuento. Se encuentra escondido en la consola`)
 
 } 
 else if (a == "helado") {
-    alert ("el producto cuesta $600")
+    alert ("el producto cuesta $" + (helado.precio))
     alert ("que lo disfrutes!")
     alert (`oh, aun no te enteraste?
     hay un codigo de descuento. Se encuentra escondido en la consola`)
@@ -59,5 +96,11 @@ if (b === `111` || b === `222`) {
    alert( "su chocolate queda en $" + c )
 } else if(b === `444`) {
     const e = 600 - (600 * 10 / 100)
-    alert( "su helado queda en $" + e )
-}
+  alert( "su helado queda en $" + e )
+} else if (b /= `111`, `222`, `333`, `444`) 
+    alert("usted no ha ingresado ningun codigo")
+
+
+//-------------------aqui los aportes de mi gato-------------------
+
+//"saaaaaaaa"
